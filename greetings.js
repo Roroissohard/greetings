c2ad4ee0f31893f556dd7f8989586b5cc8f7ebe8
Romier Manning <126rmanning@frhsd.com>
2024-10-30 13:31:02 -0400

    first commit

---
+++ greetings.js
@@ -0,0 +1,23 @@
+const d = new Date();
+let hour = d.getHours();
+hour = 20;
+let greetingText = document.getElementById("greeting-text");
+if (hour < 12) {
+
+  greetingText.textContent = "Good Morning!";
+  document.querySelector(".night").classList.add("d-none");
+  document.querySelector(".afternoon").classList.add("d-none");
+} else if (hour < 17) {
+
+  greetingText.textContent = "Good Afternoon!";
+  document.getElementsByTagName("body")[0].classList.add("noon-bg");
+  greetingText.parentElement.classList.add("noon-gradient");
+  document.querySelector(".morning").classList.add("d-none");
+  document.querySelector(".night").classList.add("d-none");
+}
+else {
+  greetingText.textContent = "Good Night!";
+  greetingText.parentElement.classList.add("night-gradient");
+  document.querySelector(".morning").classList.add("d-none");
+  document.querySelector(".afternoon").classList.add("d-none");
+}
